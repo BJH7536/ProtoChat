c316135ab57efb7131f0e4e0a5e12566d2d23856
BJH <114412598+BJH7536@users.noreply.github.com>
2023-06-02 19:01:37 +0900

    23.06.02

---
+++ Assets/Scripts/UI/Popup/Popup_Chat/AreaScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaScript : MonoBehaviour
+{
+    public RectTransform AreaRect, BoxRect, TextRect;
+    public GameObject Tail;
+    public TextMeshProUGUI TimeText, UserText, DateText;
+    public Image UserImage;
+    public string Time, User;
+}
